2fdf7dd655fdb40631dd20d9f01542e846b7ced9
vitrify <104696303+vitrifyit@users.noreply.github.com>
2022-04-30 13:19:35 -0700

    Create Problem 263 A, Beautiful Matrix.cpp

---
+++ Problem 263 A, Beautiful Matrix.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    //O(n^2) time complexity
+    int n, row, col, location;
+    for(int i = 1; i <=5; i++){
+        for(int j = 1; j <=5; j++){
+            cin >> n;
+            if(n == 1){
+                row = i;
+                col = j;
+            }
+        }
+        location = abs(3 - rows) + abs(3 - col);
+    }
+    cout << loc;
+    return 0;
+}
