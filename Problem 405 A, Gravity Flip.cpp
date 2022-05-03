92d579361b8a9deb49cc1be0f0a4427382f476bd
vitrify <104696303+vitrifyit@users.noreply.github.com>
2022-05-02 20:09:36 -0700

    Create Problem 405 A, Gravity Flip.cpp

---
+++ Problem 405 A, Gravity Flip.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int col, temp = 0;
+    int n[10000];
+    cin >> col;
+    for(int i = 0; i < col; i++){
+        cin >> n[i];
+    }
+    sort(n, n + col);
+    for(int i = 0; i < col; i++){
+        cout << n[i] << " ";
+    }
+
+    return 0;
+}
