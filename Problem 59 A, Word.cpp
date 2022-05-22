7e6f1e0221366c4e25260883af0fa465001a975c
vitrify <104696303+vitrifyit@users.noreply.github.com>
2022-05-22 16:53:44 -0700

    Create Problem 59 A, Word.cpp
    just syntax not 2 hard

---
+++ Problem 59 A, Word.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <cctype>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    string str1;
+    int upper = 0, lower = 0;
+    cin >> str1;
+    for(int i = 0; i < str1.size(); i++){
+        if(isupper(str1[i])){
+            upper++;
+        }
+        else{
+            lower++;
+        }
+    }
+    if(upper > lower){
+        char str2;
+        for(int i = 0; i < str1.size(); i++){
+            str2 = toupper(str1[i]);
+            cout << str2;
+        }
+        }else{
+            char str2;
+            for(int i = 0; i < str1.size(); i++){
+                str2 = tolower(str1[i]);
+                cout << str2;
+            }
+        }
+        return 0;
+    }
+
+
